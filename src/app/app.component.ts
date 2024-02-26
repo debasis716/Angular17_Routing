d77be33f09d4ee29ff6781ad5f81c6c693d63c6c
Debasis716 <debasis716@gmail.com>
2024-02-26 12:27:31 +0530

    ds

---
+++ src/app/app.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+
+import { CommonModule } from '@angular/common';
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA }      from '@angular/core';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+
+// import { HomeComponent } from './home/home.component';
+// import { AboutComponent } from './about/about.component';
+// import { ContactComponent } from './contact/contact.component';
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [CommonModule,RouterOutlet,NavBarComponent],
+  schemas: [ CUSTOM_ELEMENTS_SCHEMA ],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css',
+  
+})
+export class AppComponent {
+  title = 'AngIntro';
+  cData:any;
+
+getData(val:any){
+this.cData=val;
+}
+}
---
+++ src/app/app.routes.ts
@@ -0,0 +1,14 @@
+import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+
+export const routes: Routes = [
+
+    { path: '','title':'Home', component: HomeComponent },
+    { path: 'about', 'title':'About',component: AboutComponent },
+    { path: 'contact','title':'Contact', component: ContactComponent },
+    {path: '**', component: PageNotFoundComponent}
+
+];
